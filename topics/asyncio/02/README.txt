9121e6d45cfebd5f5e81661fe0b4c03fc33494da
tannerj <codingfrogs@yahoo.com>
2024-07-26 16:34:34 +0100

    feat: Add blocking multi-client server problem example

---
+++ topics/asyncio/02/README.txt
@@ -0,0 +1,4 @@
+README.txt
+
+Example shows the principle blocking problem!
+We can't seem to server more than one client as the listening call blocks!
---
+++ topics/asyncio/02/client-sequential.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# Echo client program
+import socket
+import time
+
+
+class PrimeClient:
+    """Prime client"""
+
+    def __init__(self):
+        self.socket = None
+        self.HOST = 'localhost'    # The remote host
+        self.PORT = 50007          # The same port as used by the server
+
+    def run(self):
+        self.setup_socket()
+        self.connect_to_server()        
+        self.get_primes()
+        self.clean_up()
+
+    def setup_socket(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       
+
+    def connect_to_server(self):
+        self.socket.connect((self.HOST, self.PORT))
+        print("Connected!")
+
+    def get_primes(self):
+        for i in range(4):
+            prime = self.get_prime()
+            print(prime)            
+
+    def get_prime(self):
+        self.socket.sendall(b'get')
+        data: bytes = self.socket.recv(4)  # Get value
+        value: int = int.from_bytes(data, 'big')
+        return value 
+
+    def clean_up(self):        
+        self.close_socket()
+
+    def close_socket(self):
+        self.socket.close()
+
+def main():
+    
+    prime_client = PrimeClient()
+    prime_client.run()
+
+if __name__ == "__main__":
+    main()
