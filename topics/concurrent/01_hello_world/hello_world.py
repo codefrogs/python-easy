7fb45c7dde008ef239e72dfee56ef062ed89b901
tannerj <codingfrogs@yahoo.com>
2024-08-02 10:28:55 +0100

    feat: add multi-processing helloworld example

---
+++ topics/concurrent/01_hello_world/hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from multiprocessing import Process
+
+def hello():
+    print("Hello world!")
+
+def main():
+    p = Process(target=hello)
+    
+    # Nothing happens yet, we need to start the process
+    p.start()
+    p.join()
+
+if __name__ == "__main__":
+    main()
