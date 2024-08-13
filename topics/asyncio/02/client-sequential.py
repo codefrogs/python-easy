87f47d5026f771cb7094b154e693c19884bcc1b0
tannerj <codingfrogs@yahoo.com>
2024-08-13 14:17:45 +0100

    refactor: Increase number of 'gets' by client

--- topics/asyncio/02/client-sequential.py
+++ topics/asyncio/02/client-sequential.py
@@ -14,38 +14,40 @@ class PrimeClient:
 
     def run(self):
         self.setup_socket()
-        self.connect_to_server()        
+        self.connect_to_server()
         self.get_primes()
         self.clean_up()
 
     def setup_socket(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect_to_server(self):
         self.socket.connect((self.HOST, self.PORT))
         print("Connected!")
 
     def get_primes(self):
-        for i in range(4):
+        for i in range(20):
             prime = self.get_prime()
-            print(prime)            
+            print(prime)
 
     def get_prime(self):
         self.socket.sendall(b'get')
         data: bytes = self.socket.recv(4)  # Get value
         value: int = int.from_bytes(data, 'big')
-        return value 
+        return value
 
-    def clean_up(self):        
+    def clean_up(self):
         self.close_socket()
 
     def close_socket(self):
         self.socket.close()
 
+
 def main():
-    
+
     prime_client = PrimeClient()
     prime_client.run()
+
 
 if __name__ == "__main__":
     main()
